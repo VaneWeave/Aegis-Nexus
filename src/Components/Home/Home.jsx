2e82351df9f87bcbfa19052fdb2c0e093ae3bac0
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-02-11 01:31:56 +0530

    Tailwind CSS Connected

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src="https://img.freepik.com/free-vector/medical-workers-analyzing-electronic-record_1262-19834.jpg?w=2000&t=st=1676055889~exp=1676056489~hmac=9f846495f0e5ccde9dfc7be12826a3d92b8d56bbf34a33e98d1b032001c8e3dc" className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Welcome to <span className='font-[100]'>Aegis</span><span className='text-[#e34d1c]'>-</span><span className='font-black text-success'>Nexus</span>, the solution to fake certificates in the healthcare industry. Trust only qualified professionals with our cutting-edge verification technology. Improve healthcare quality and ensure patient safety. Start using <span className='font-[100]'>Aegis</span><span className='text-[#e34d1c]'>-</span><span className='font-black text-success'>Nexus</span> today.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+</div>
+
+  )
+}
+
+export default Home
